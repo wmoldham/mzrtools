d53843cdb6d817c0ec5cf31bee02a9c2ad6f569d
William Oldham <44209581+wmoldham@users.noreply.github.com>
2020-10-15 15:23:51 -0400

    add mz_iso_annotate function

---
+++ R/mz_iso_annotate.R
@@ -0,0 +1,53 @@
+#' Annotate isotope table
+#'
+#' \code{mz_iso_annotate} generates a tibble enumerating all possible isotopes of
+#'     a molecule and annotates this with the mass or m/\emph{z} of each isotope
+#'     based on the polarity (pol) setting and the nominal mass shift of the isotope.
+#'
+#' @inheritParams mz_atomize
+#' @inheritParams mz_calculate
+#'
+#' @return A tibble of isotope information
+#'
+#' @export
+#'
+#' @examples
+#' mz_iso_annotate("C5H8O5")
+#' mz_iso_annotate("C5H8O5", pol = "negative")
+#'
+mz_iso_annotate <- function(mol, pol = "negative") {
+
+  if (!(pol %in% c("neutral", "positive", "negative"))) {
+    warning("Unrecognized polarity, neutral masses returned.")
+  }
+
+  elements <- mz_atomize(mol)
+  isotopes <- elements[names(elements) %in% names(iso_info)]
+
+  # get unlabeled mass
+  no_label <- elements[names(elements) %nin% names(iso_info)]
+  if (length(no_label) == 0) {
+    unlabeled_mass <- 0
+  } else {
+    no_label <- stringr::str_c(names(no_label), no_label, collapse = "")
+    unlabeled_mass <- mz_calculate(no_label, pol = pol)
+  }
+
+  # get isotope information
+
+  # extractor function for iso_info
+  annot <- function(sublist, iso_list) {
+    out <- unlist(lapply(iso_info[names(isotopes)], "[[", sublist))
+    names(out) <- unlist(lapply(iso_info[names(isotopes)], "[[", "isotope"))
+    iso_list %*% out
+  }
+
+  iso_list <- mz_iso_list(mol)
+  iso_mass <- c(annot("mass", iso_list) + unlabeled_mass)
+  iso_shift <- c(annot("shift", iso_list))
+
+  tibble::as_tibble(iso_list) %>%
+    dplyr::bind_cols(mass = iso_mass, shift = iso_shift) %>%
+    dplyr::arrange(mass)
+
+}
---
+++ tests/testthat/test-mz_iso_annotate.R
@@ -0,0 +1,3 @@
+test_that("warning works", {
+  expect_warning(mz_iso_annotate("C3", pol = "x"))
+})
